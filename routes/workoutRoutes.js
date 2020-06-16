47db5d126243b475961b55343c2ec17a06652801
Michael Tirona <tironamichael@gmail.com>
2020-06-15 21:46:32 -0700

    Added routes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const { Workout } = require('../models')
+const { join } = require('path')
 
+// Find all workouts
 router.get('/workout', (req, res) => {
     Workout.find()
     .then((workout) => {
@@ -9,12 +11,45 @@ router.get('/workout', (req, res) => {
         .catch(err => console.error(err))
 })
 
+// Post a workout
 router.post('/workouts', (req, res) => {
     Workout.create(req.body)
         .then(workout => res.json(workout))
         .catch(err => console.error(err))
 })
 
+// Find one workout
 router.get('workouts/:id', (req, res) => {
-    
-}
+    Workout.findById(req.params.id)
+        .then(workout => res.json(workout))
+        .catch(err => console.error(err))
+})
+
+// Update a workout
+router.get('/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body}})
+        .then(workout => res.json(workout))
+        .catch(err => console.error(err))
+})
+
+// Delete a workout 
+router.get('/workout/:id', (req, res) => {
+    Workout.findByIdAndDelete(req.params.id)
+        .then(() => res.sendStatus(200))
+        .catch(err => console.error(err))
+})
+
+//Route to index html file
+router.get('/', (req, res) => {
+    res.sendFile(join(__dirname. '../public/index.html'))
+})
+
+// Route to exercise HTML file
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+})
+
+// Route to stats html file
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { join } = require('path')
 const app = express()
 
-app.use(express.static(join(_dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
